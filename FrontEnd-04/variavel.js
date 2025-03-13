25eec850fcc65b540e48d9fb5c77d8be6af75696
Dandan426 <danielanunesmorai23@gmail.com>
2025-03-13 10:19:04 -0300

    Adicionando frontend 04 onde criamos e imprimimos variaveis

---
+++ FrontEnd-04/variavel.js
@@ -0,0 +1,9 @@
+// Criando variáveis 
+let nome = "Dandan"; // String
+let idade = 16; // Int
+let solteira = true; // Booleano
+
+// Imprimindo variáveis
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof solteira);
