9bf3f4268fa2f6b4135f08cdc1ceafae114c0b84
Dandan426 <danielanunesmorai23@gmail.com>
2025-03-13 10:22:20 -0300

    Adicionando frontend 03 onde criamos variavel para digitar numeros

--- 20-02/variavel.js
+++
@@ -1,9 +0,0 @@
-// Criando variáveis 
-let nome = "Dandan"; // String
-let idade = 16; // Int
-let solteira = true; // Booleano
-
-// Imprimindo variáveis
-console.log(typeof nome);
-console.log(typeof idade);
-console.log(typeof solteira);
